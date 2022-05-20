ed979ef0bc42b7a99d50161ea59edc8aae432f49
kyojro <rostik.kuharchuk@gmail.com>
2022-05-20 22:08:05 +0300

    second commit

--- pw_5.py
+++ pw_5.py
@@ -2,27 +2,34 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 import datetime
+import threading
+
+
+def clear_file(name):
+    r = open(name, "w")
+    r.write(f"product that is cheaper {sys.argv[1]}" + "\n" + "\n")
+    r.close()
+
+
+t = threading.Thread(target=clear_file, args=("result", ))
+t.start()
+t.join()
 
 
 class Paraer:
 
     def __init__(self, url):
         # "https://rozetka.com.ua/ua/shoes_clothes/c1162030/producer=nike/"
+        self.price2 = None
         self.url = requests.get(url).text
         self.soup = BeautifulSoup(self.url, "html.parser")
         self.time_1 = datetime.datetime.now()
 
-    @staticmethod
-    def clear_file():
-        r = open("result", "w")
-        r.write(f"product that is cheaper {sys.argv[1]}" + "\n" + "\n")
-        r.close()
-
     def search(self):
         for value in self.soup.find_all("div", class_="goods-tile__inner"):
             price = value.find("span", class_="goods-tile__price-value").text
-            price2 = price.replace('\xa0', '')
-            if int(price2) < int(sys.argv[1]):
+            self.price2 = price.replace('\xa0', '')
+            if int(self.price2) < int(sys.argv[1]):
                 href = value.find("a").get("href")
                 req = requests.get(href + "characteristics/").text
                 soup2 = BeautifulSoup(req, "html.parser")
@@ -37,8 +44,12 @@ class Paraer:
                 time_all = time_2 - self.time_1
                 with open("result", "a") as f:
                     f.write(f"time_work: {str(time_all)}" + '\n')
+    
+    def info(self):
+        if int(self.price2) >= int(sys.argv[1]):
+            print("There are no products matching your request")
 
 
 parser = Paraer("https://rozetka.com.ua/ua/shoes_clothes/c1162030/producer=nike/")
-parser.clear_file()
 parser.search()
+parser.info()
